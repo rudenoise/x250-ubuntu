65038236f811716fe423c7d0e13ad67da227c07c
rudenoise <rudenoise@gmail.com>
2016-03-31 15:01:54 +0100

    get things stated

---
+++ git.sh
@@ -0,0 +1,7 @@
+apt-get install -y git
+git config --global user.name "rudenoise"
+git config --global user.email rudenoise@gmail.com
+ssh-keygen -t rsa -b 4096 -C "rudenoise@gmail.com"
+sudo apt-get install xclip
+xclip -sel clip < ~/.ssh/id_rsa.pub
+echo "an ssh key has been copied to your clipboard paste it into github"
---
+++ ocra.sh
@@ -0,0 +1,1 @@
+apt-get install fonts-ocr-a
