fcf435c17da32473114dd8870e2ab71eb22fd0fb
rudenoise <rudenoise@gmail.com>
2016-04-01 10:56:25 +0100

    add i3wm startup scruipt

--- mouse-setup.sh
+++ mouse-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mkdir ~/bin
+mkdir -p ~/bin
 cp mouse.sh ~/bin/
 chmod +x ~/bin/mouse.sh
-cp mouse.desktop ~/config/autostart/
+cp mouse.desktop ~/.config/autostart/
+
+echo "exec ~/bin/mouse.sh" >> ~/i3/config
