603bd3e46c7c4cde4fff80e62095abf077716a61
rudenoise <rudenoise@gmail.com>
2016-06-24 14:15:56 +0100

    add battery stuff

--- bits-n-peices.sh
+++ bits-n-peices.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+sudo apt-add-repository ppa:morgwai/tpbat
+sudo apt-get update
+
 sudo apt-get install -y pandoc
 sudo apt-get install -y redshift
-sudo apt-get install -y tlp tlp-rdw 
+sudo apt-get install -y tlp tlp-rdw tpacpi-bat
