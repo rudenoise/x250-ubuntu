8da5fa7d01c3d77f2caed8d72222e3bf95ea16b6
rudenoise <rudenoise@gmail.com>
2016-07-11 21:37:57 +0100

    switch to docker install script

--- docker-env.sh
+++ docker-env.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo touch /etc/apt/sources.list.d/docker.list
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get purge lxc-docker
-sudo apt-cache policy docker-engine
-sudo apt-get install linux-image-extra-$(uname -r)
-sudo apt-get install docker-engine
-sudo gpasswd -a ${USER} docker
-service docker start
-docker run hello-world
-apt-get install -y curl
-sudo -i
-curl -L https://github.com/docker/compose/releases/download/1.7.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-exit
+curl -sSL https://get.docker.com | sh
+
+#sudo apt-get install apt-transport-https ca-certificates
+#sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#sudo touch /etc/apt/sources.list.d/docker.list
+#sudo echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >> /etc/apt/sources.list.d/docker.list
+#sudo apt-get update
+#sudo apt-get purge lxc-docker
+#sudo apt-cache policy docker-engine
+#sudo apt-get install linux-image-extra-$(uname -r)
+#sudo apt-get install docker-engine
+#sudo gpasswd -a ${USER} docker
+#service docker start
+#docker run hello-world
+#apt-get install -y curl
+#sudo -i
+#curl -L https://github.com/docker/compose/releases/download/1.7.0-rc1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+#chmod +x /usr/local/bin/docker-compose
+#exit
